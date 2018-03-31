9d2fc86578d4a1bdd06a65c38c004c0ecc3ec8ea
Kimberly Ding <dingkimberly@nat-oitwireless-inside-vapornet100-c-11141.Princeton.EDU>
2018-03-31 00:38:11 -0400

    Favicon

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
+app.use('/favicon.ico', express.static('images/favicon.ico'));
+
 app.use(express.static(__dirname + '/public'));
 
 app.set('views', __dirname + '/views');
