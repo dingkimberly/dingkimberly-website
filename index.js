abaa9cc63edbd7823b98da4947580e1e00ad2f21
Kimberly Ding <dingkimberly@nat-oitwireless-inside-vapornet100-c-11141.Princeton.EDU>
2018-03-31 04:49:12 -0400

    Intro to CSS taught mostly by w3schools.com to be honest

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ var favicon = require('express-favicon');
 
 app.set('port', (process.env.PORT || 5000));
 
+app.use("/files", express.static(__dirname + '/files')); 
 app.use("/images", express.static(__dirname + '/images')); 
+app.use("/stylesheets", express.static(__dirname + '/stylesheets')); 
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
