a415fc32cdb681d1391e9d2ed3947154efe42e37
Kimberly Ding <dingkimberly@nat-oitwireless-inside-vapornet100-c-11141.Princeton.EDU>
2018-03-31 22:07:49 -0400

    Misc stuff added

--- index.js
+++ index.js
@@ -1,24 +1,54 @@
 var express = require('express');
 var app = express();
 
-var favicon = require('express-favicon');
+/*-----------------------------------------------------------------*/
 
 app.set('port', (process.env.PORT || 5000));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+/*-----------------------------------------------------------------*/
 
 app.use("/files", express.static(__dirname + '/files')); 
 app.use("/images", express.static(__dirname + '/images')); 
 app.use("/stylesheets", express.static(__dirname + '/stylesheets')); 
 
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+/*-----------------------------------------------------------------*/
 
 app.get('/', function(request, response) {
 	response.render('index')
 });
 
+app.get('/button', function(request, response) {
+	response.render('button')
+});
+
 app.get('/secrets', function(request, response) {
 	response.render('secrets')
 });
+
+/*-----------------------------------------------------------------*/
+
+// Connect to Postgres
+
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
+
+/*-----------------------------------------------------------------*/
 
 app.listen(app.get('port'), function() {
 	console.log('Node app is running on port', app.get('port'));
